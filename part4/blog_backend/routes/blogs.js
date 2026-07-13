838969c95dcf35957e45d6df03a8b66439d8ece0
imKokoT <prikolkillercat@gmail.com>
2026-07-13 19:11:26 +0200

    /api/blog/:id put

--- part4/blog_backend/routes/blogs.js
+++ part4/blog_backend/routes/blogs.js
@@ -34,6 +34,25 @@ router.delete('/:id', async (req, res) => {
     return res.status(404).end()
 
   logger.info('DELETE Blog ID', id)
+  res.status(204).end()
+})
+
+router.put('/:id', async (req, res) => {
+  const id = req.params.id
+  const newBlog = req.body
+
+  const blog = await Blog.findById(id)
+  if (!blog)
+    return res.status(404).end()
+
+  blog.title = newBlog.title
+  blog.author = newBlog.author
+  blog.url = newBlog.url
+  blog.likes = newBlog.likes
+
+  await blog.save()
+  logger.info('UPDATED Blog ID', id)
+  res.status(200).json(blog)
 })
 
 module.exports = router
