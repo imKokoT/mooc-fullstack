10fe29194890f5a90df2577491a70e21adf56488
imKokoT <prikolkillercat@gmail.com>
2026-07-17 19:11:30 +0200

    forgot few checks while signup

--- part4/blog_backend/routes/signup.js
+++ part4/blog_backend/routes/signup.js
@@ -7,13 +7,19 @@ const SALT_ROUNDS = 10
 
 
 router.post('/', async (req, res) => {
-  const { username, password } = req.body
+  const { username, password } = {
+    username: req.body.username.trim(),
+    password: req.body.password.trim()
+  } 
+
+  if(username.length < 3)
+    return res.status(400).json( {error: 'username must be at least 3 characters long'})
+  if(password.length < 3)
+    return res.status(400).json( {error: 'password must be at least 3 characters long'})
 
   const user = await User.findOne({ username })
-  if (user) {
-    res.status(409).json({ error: 'user already exists' })
-    return
-  }
+  if (user)
+    return res.status(409).json({ error: 'user already exists' })
 
   const newUser = new User({
     username: username,
