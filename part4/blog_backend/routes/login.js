b8966280a093499be3d5ee4574b7626f191838ee
imKokoT <prikolkillercat@gmail.com>
2026-07-17 19:03:18 +0200

    /api/login

--- part4/blog_backend/routes/login.js
+++ part4/blog_backend/routes/login.js
@@ -1,10 +1,39 @@
 const router = require('express').Router()
 const User = require('../models/user')
-const logger = require('../utils/logger')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 router.post('/', async (req, res) => {
-  res.sendStatus(501)
+  const { username, password } = req.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.password)
+
+  if (!(user && passwordCorrect)) {
+    return res.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  }
+
+  const token = jwt.sign(
+    userForToken, process.env.SECRET,
+    { expiresIn: 60*60 }
+  )
+
+  user.last_login = Date.now()
+  user.save()
+
+  res
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
 })
 
 module.exports = router
