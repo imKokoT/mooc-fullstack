22273152b7df184fc78dfe894a228631e7939999
imKokoT <prikolkillercat@gmail.com>
2026-07-19 19:06:18 +0200

    get /api/users & /api/users/:id

--- part4/blog_backend/routes/users.js
+++ part4/blog_backend/routes/users.js
@@ -1,14 +1,25 @@
 const router = require('express').Router()
 const User = require('../models/user')
-const logger = require('../utils/logger')
 
 
 router.get('/', async (req, res) => {
-  res.sendStatus(501)
+  const users = await User.find({})
+    .select('-password')
+    .populate('blogs')
+  
+  res.json(users)
 })
 
 router.get('/:id',async (req, res) => {
-  res.sendStatus(501)
+  const id = req.params.id
+
+  const user = await User.findById(id)
+    .select('-password')
+    .populate('blogs')
+  if (!user)
+    return res.status(404).end()
+
+  res.json(user)
 })
 
 module.exports = router
