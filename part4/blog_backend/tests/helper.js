927c7180952820bccb42431ea3624636682a8ccb
imKokoT <prikolkillercat@gmail.com>
2026-07-19 19:28:16 +0200

    updated tests' helper

--- part4/blog_backend/tests/helper.js
+++ part4/blog_backend/tests/helper.js
@@ -1,59 +1,69 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-const blogs = [
-  { 
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-  },
-  { 
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  },
-  { 
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
+const usersSignup = [
+  {
+    username: 'Michael Chan',
+    password: 'abc',
+    blogs: [
+      { 
+        title: 'React patterns',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+      },
+    ]
   },
   {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-  },
-  { 
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
+    username: 'Edsger W. Dijkstra',
+    password: 'cba',
+    blogs: [
+      { 
+        title: 'Go To Statement Considered Harmful',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+      },
+      { 
+        title: 'Canonical string reduction',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+      },
+    ]
   },
   {
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
+    username: 'Robert C. Martin',
+    password: '123',
+    blogs: [
+      {
+        title: 'First class tests',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+        likes: 10,
+      },
+      { 
+        title: 'TDD harms architecture',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+        likes: 0,
+      },
+      {
+        title: 'Type wars',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2,
+      }
+    ]
   }
 ]
-
-const nonExistingId = async () => {
-  const blog = new Blog({ content: 'willremovethissoon' })
-  await blog.save()
-  await blog.deleteOne()
-
-  return blog._id.toString()
-}
 
 const fetchBlogs = async () => {
   const _blogs = await Blog.find({})
   return _blogs.map((note) => note.toJSON())
 }
 
+const fetchUsers = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
 module.exports = {
-  blogs,
-  nonExistingId,
-  fetchBlogs: fetchBlogs
+  usersSignup,
+  fetchBlogs,
+  fetchUsers
 }
