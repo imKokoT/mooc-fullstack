7827136298c9f93b6eb35ecab54f983f7c19abe6
imKokoT <prikolkillercat@gmail.com>
2026-07-24 20:05:42 +0200

    prepared minimal template

--- part5/blog_frontend/src/App.jsx
+++ part5/blog_frontend/src/App.jsx
@@ -1,11 +1,23 @@
-
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
 
 function App() {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
 
   return (
-    <>
-      Hello world
-    </>
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
   )
 }
 
