c024bd36ec526bc1e66f0081fd62409ed319df1c
imKokoT <prikolkillercat@gmail.com>
2026-07-26 12:04:39 +0200

    improved Blog component

--- part5/blog_frontend/src/components/Blog.jsx
+++ part5/blog_frontend/src/components/Blog.jsx
@@ -1,8 +1,40 @@
+import { useState } from 'react'
+import './Blog.css'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+function Blog({ blog }) {
+  const [showDetails, setShowDetails] = useState(false)
+  
+  const hideWhenVisible = { display: showDetails ? 'none' : '' }
+  const showWhenVisible = { display: showDetails ? '' : 'none' }
+
+  function switchDetails() {
+    setShowDetails(!showDetails)
+  }
+
+  return (
+    <div className="blog">
+      {/* shorten view */}
+      <div style={hideWhenVisible}>
+        {blog.title} By {blog.owner.username}
+        <button className='button' onClick={switchDetails}>View</button>
+      </div>
+
+      {/* expanded view */}
+      <div style={showWhenVisible}>
+        {blog.title} 
+        <button className='button' onClick={switchDetails}>Hide</button>
+        <br />
+
+        {blog.url} <br /> 
+
+        likes: {blog.likes} 
+        <button className='button'>like</button>
+        <br />
+        
+        By {blog.owner.username} <br />
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
