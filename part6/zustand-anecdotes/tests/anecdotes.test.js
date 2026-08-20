e8710b51bb6607ac884e415955954dbe0312881d
imKokoT <prikolkillercat@gmail.com>
2026-08-20 11:49:19 +0200

    done 6.6-6.15
    other tests

--- part6/zustand-anecdotes/tests/anecdotes.test.js
+++ part6/zustand-anecdotes/tests/anecdotes.test.js
@@ -8,7 +8,7 @@ vi.mock('../src/services/anecdotes', () => ({
   default: {
     getAll: vi.fn(),
     addNew: vi.fn(),
-    voteUp: vi.fn(),
+    update: vi.fn(),
   }
 }))
 
@@ -33,14 +33,51 @@ describe('test anecdotes store', () => {
   })
 
   it('display sorted anecdotes by votes from the store', async () => {
+    const mockAnecdotes = [
+      { id: 1, content: 'Test', votes: 5 },
+      { id: 2, content: 'Test 2', votes: 1 },
+      { id: 3, content: 'Test 3', votes: 2 },
+      { id: 4, content: 'Test 4', votes: 7 },
+    ]
+    useAnecdoteStore.setState({ anecdotes: mockAnecdotes })
 
+    const { result: anecdotesResult } = renderHook(() => useAnecdotes())
+    expect(anecdotesResult.current).toEqual(mockAnecdotes.toSorted((a, b) => b.votes - a.votes))
   })
 
   it('filter tests', async () => {
+    const mockAnecdotes = [
+      { id: 4, content: 'foo', votes: 7 },
+      { id: 1, content: 'bar', votes: 5 },
+      { id: 3, content: 'bar Foo', votes: 2 },
+      { id: 2, content: 'baz', votes: 1 },
+    ]
+    useAnecdoteStore.setState({anecdotes: mockAnecdotes})
+    
+    const { result } = renderHook(() => useAnecdoteActions())
+    await act(async () => {
+      await result.current.setFilter('foo')
+    })
 
+    const { result: anecdotesResult } = renderHook(() => useAnecdotes())
+    expect(anecdotesResult.current).toEqual([
+      { id: 4, content: 'foo', votes: 7 },
+      { id: 3, content: 'bar Foo', votes: 2 },
+    ])
   })
 
   it('vote up an anecdote', async () => {
+    const anecdote = { id: 1, content: 'Test', votes: 5 }
+    useAnecdoteStore.setState({anecdotes: [anecdote]})
+    AnecdotesService.update.mockResolvedValue({ ...anecdote, votes:6 })
 
+    const { result } = renderHook(() => useAnecdoteActions())
+    await act(async () => {
+      await result.current.voteUp(1)
+    })
+
+    const { result: anecdotesResult } = renderHook(() => useAnecdotes())
+    console.log(anecdotesResult.current)
+    expect(anecdotesResult.current[0].votes).toBe(6)
   })
 })
